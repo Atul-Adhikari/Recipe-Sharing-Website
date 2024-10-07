e7a33ad335b4a4a6700e6f965bed3ca89c36b119
Atul Adhikari <143103714+Atul-Adhikari@users.noreply.github.com>
2024-10-07 14:21:47 +0545

    Update urls.py

--- foodapp_backend/urls.py
+++ foodapp_backend/urls.py
@@ -11,6 +11,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('recipes.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
